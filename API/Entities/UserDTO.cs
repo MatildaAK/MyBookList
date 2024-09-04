5bd2ef544c9b24eaaecf2ebf24c760e63bb410a9
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-04 09:07:27 +0200

    Entities

---
+++ API/Entities/UserDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class UserDTO
+    {
+        public required string Name { get; set; }
+        public string? UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }  
+    }
+}
